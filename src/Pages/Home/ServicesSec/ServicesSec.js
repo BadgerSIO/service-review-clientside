8d24d3c46d60ce05375ee446ae8afbb18200edda
BadgerSIO <saadibneomar@gmail.com>
2022-11-09 15:45:53 +0600

    services loaded at service serction

--- src/Pages/Home/ServicesSec/ServicesSec.js
+++ src/Pages/Home/ServicesSec/ServicesSec.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../../../context/AuthProvider";
 
 const ServicesSec = () => {
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:5000/lastServices`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  }, [user?.email]);
   return (
     <div className="py-20 container border-b border-b-borderTheme">
       <h1 className="text-5xl font-titles">Our Service area includes</h1>
